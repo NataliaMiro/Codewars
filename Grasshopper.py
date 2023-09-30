e012fc6f64754d292456b3e2fa6183dcb2f1192b
NataliaMiro <124344377+NataliaMiro@users.noreply.github.com>
2023-09-30 09:32:25 +0300

    added Grasshopper.py

---
+++ Grasshopper.py
@@ -0,0 +1,21 @@
+```
+Grasshopper - Variable Assignment Debug (retired)
+Variable assignment
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+Fundamentals
+a == "dev"
+b == "Lab"
+
+name == a + b
+```
+
+a = "dev"
+b = "Lab"
+
+name = f'{a}{b}'
+
+
+a = "dev"
+b = "Lab"
+
+name = a + b
